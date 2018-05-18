7fb76cba3a0534eebf01ceee6b959263cf284706
Allison Hahn <allisonk.hahn@gmail.com>
2018-05-17 17:20:18 -0700

    most javascript in place

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,12 +5,78 @@ var questions= [
     },
     {question: "What does Dunder Mifflin sell?",
     choices: ["cars", "caskets", "paper", "mufflers"],
-    answer: 2}
+    answer: 2},
+
+    {question: "Who does Michael sing 'You sexy Thing' to at the Dundies?",
+    choices: ["Ryan", "Pam", "Jan", "Jim"],
+    answer: 0}
 ];
 
-for ( var i = 0; i < questions.length; i++ ) {
-    var question = questions[i].question;
-    $("#question").html(question);
-    var options = questions[i].choices;
-    $("#options").html(options);
+var images = ["lost.gif", "../assets/images/won.gif"]
+
+$(document).ready(function() {    
+    start();
+});
+
+var questionOrder=0
+var countdowntimer;
+
+function start() {
+        countdowntimer=null;
+            var q = questions[questionOrder].question;
+            $("#question").html(q);
+            var options = questions[questionOrder].choices;
+            var answer = questions[questionOrder].answer;
+            var wrong = 1;
+            for (var x = 0; x < options.length; x++) {
+                if (x==answer) {
+                    $("#right").html(options[answer])
+                }
+                else {
+                    $("#wrong"+wrong).html(options[x])
+                    wrong++; 
+                }
+            }
+    
+            var timeleft = 11;
+            countdowntimer = setInterval(function(){
+                timeleft--;
+                $("#timer").text("seconds left:"+timeleft);
+                if(timeleft <= 0) {
+                    clearInterval(countdowntimer);
+                    $("#timer").text(" ")
+                }
+            }, 1000);
+}
+var correct=0;
+var remaining=10;
+
+
+$("#right").click(function () {
+    $("#question").html("<img src='assets/images/won.gif' />")
+    correct++;
+    score()
+});
+
+$(".wrongClick").click(function () {
+    $("#question").html("<img src='assets/images/lost.gif' />")
+    $("#winOrLoss").html("lost!");
+    score()
+});
+
+
+$("#next").click(function () {
+    questionOrder++
+    start()
+});
+
+function score () {
+    remaining--;
+    $("#correct").text(" " + correct);
+    $("#remaining"). text(" " + remaining);
+    clearInterval(countdowntimer);
+    $("#timer").html(" ");
+    $("#next").html("Next>>")
+    $("#right").text("");
+    $(".wrongClick").text("");
 }
